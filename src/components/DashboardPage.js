80580e26b0440a17052944e680f76bcfac17ef4e
Jorge Bautista <jorgebautista@gmail.com>
2020-05-19 23:53:23 -0400

    Dashboard appears only if user is logged in

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AdRows from '../components/panel/AdRows';
 import 'normalize-scss/sass/_normalize.scss';
 import '../styles/styles.scss';
@@ -6,8 +6,26 @@ import logo from '../images/logo.svg';
 import heart from '../images/icons8-heart_outline.png';
 import home from '../images/icons8-home.png';
 import default_avatar from '../images/icons8-user_male_circle_filled.png';
-
+import { getLoggedInUser } from '../backend_interface/api_if';
 const DashboardPage = () => {
+	const getUser = async () => {
+		let user = await getLoggedInUser();
+
+		return user;
+	};
+
+	const [ user, setUser ] = useState(undefined);
+	useEffect(() => {
+		getUser().then((currentUser) => setUser(currentUser));
+	}, []);
+
+	if (user === null) {
+		return <div>Unauthorized</div>;
+	}
+	if (user === undefined) {
+		console.log(user);
+		return <div>Loading...</div>;
+	}
 	return (
 		<div className="panel">
 			<div className="panel_left">
@@ -19,7 +37,7 @@ const DashboardPage = () => {
 				<div className="avatar">
 					<img src={default_avatar} alt="avatar" />
 				</div>
-				<div className="name">Jane Smith</div>
+				<div className="name">{user && user.email}</div>
 				<div className="menu">
 					<a href="" className="menu_item">
 						<img src={heart} alt="favorites" />
